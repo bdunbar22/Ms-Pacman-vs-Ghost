6dcca0badd06ccb0a371572cca62692386c57678
Ben Dunbar <dunbarb@ccs.neu.edu>
2017-02-04 17:50:46 -0500

    Starting homework 2

---
+++ src/pacman/Dijkstra/Dijkstra.java
@@ -0,0 +1,17 @@
+package pacman.Dijkstra;
+
+/**
+ * Created by Ben on 2/4/17.
+ */
+public class Dijkstra {
+
+
+    public void dijkstraAlgorithm( /* Arguments needed here */) {
+        // read file
+        // compute dijkstras
+        // write output
+    }
+
+    /* Private functions */
+
+}
