8c05c575ea4c6fdb621bf563cb6e3b4703ec66b3
Ben Dunbar <dunbarb@ccs.neu.edu>
2017-02-04 18:26:42 -0500

    Creating our own vertex class to convert the files into vertices.

--- src/pacman/Dijkstra/Dijkstra.java
+++ src/pacman/Dijkstra/Dijkstra.java
@@ -1,5 +1,12 @@
 package pacman.Dijkstra;
 
+import pacman.game.internal.Node;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Created by Ben on 2/4/17.
  */
@@ -8,7 +15,18 @@ public class Dijkstra {
 
     public void dijkstraAlgorithm( /* Accept the graph being used, start, and end node */) {
         // read file into a list of nodes
+        // We can look at new Game(0) to see how this is done. But we will
+        // convert them into our own Vertex class so it has a previous and distance value.
+
         // create node set Q
+        Set<Node> Q = new HashSet<>(); // We don't necessarily need to use node here
+        // we need an object with the following fields:
+        // OUR_NODE_OBJECT
+        //      previous
+        //      distance
+        //      List<OUR_NODE_OBJECT> neighbors
+
+
 
         // for each node:
         // set dist[v] to infinity
@@ -47,5 +65,41 @@ public class Dijkstra {
     }
 
     /* Private functions */
+
+    private class Vertex {
+        private Vertex previous;
+        private int distance;
+        private List<Vertex> neighbors;
+
+        protected Vertex() {
+            this.previous = null;
+            this.distance = Integer.MAX_VALUE;
+            this.neighbors = new ArrayList<>();
+        }
+
+        public Vertex getPrevious() {
+            return previous;
+        }
+
+        public void setPrevious(Vertex previous) {
+            this.previous = previous;
+        }
+
+        public int getDistance() {
+            return distance;
+        }
+
+        public void setDistance(int distance) {
+            this.distance = distance;
+        }
+
+        public List<Vertex> getNeighbors() {
+            return neighbors;
+        }
+
+        public void setNeighbors(List<Vertex> neighbors) {
+            this.neighbors = neighbors;
+        }
+    }
 
 }
