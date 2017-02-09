bf9d87b4d6e8198dbe3766fbcb2dbdb6bac9e29c
Ben Dunbar <dunbarb@ccs.neu.edu>
2017-02-09 02:45:34 -0500

    Starting A* problem

--- src/pacman/entries/ghosts/MyGhosts.java
+++ src/pacman/entries/ghosts/MyGhosts.java
@@ -2,9 +2,11 @@ package pacman.entries.ghosts;
 
 import java.util.EnumMap;
 import pacman.controllers.Controller;
+import pacman.game.Constants;
 import pacman.game.Constants.GHOST;
 import pacman.game.Constants.MOVE;
 import pacman.game.Game;
+import pacman.game.internal.Node;
 
 /*
  * This is the class you need to modify for your entry. In particular, you need to
@@ -13,14 +15,58 @@ import pacman.game.Game;
  */
 public class MyGhosts extends Controller<EnumMap<GHOST,MOVE>>
 {
+	private final static int PILL_PROXIMITY = 12;		//if Ms Pac-Man is this near a power pill, run
+
 	private EnumMap<GHOST, MOVE> myMoves=new EnumMap<GHOST, MOVE>(GHOST.class);
 	
 	public EnumMap<GHOST, MOVE> getMove(Game game, long timeDue)
 	{
 		myMoves.clear();
-		
-		//Place your game logic here to play the game as the ghosts
+		int pacManCurrentNodeIndex = game.getPacmanCurrentNodeIndex();
+		int ghostCurrentNodeIndex;
+
+		for(GHOST ghost : GHOST.values())	//for each ghost
+		{
+			//NOTE: make own require action so that ghosts can turn around
+			if(game.doesGhostRequireAction(ghost))		//if ghost requires an action
+			{
+				// Reuse existing code for running away
+				if(game.getGhostEdibleTime(ghost) > 0 || closeToPower(game))
+					myMoves.put(ghost,game.getApproximateNextMoveAwayFromTarget(game.getGhostCurrentNodeIndex(ghost),
+						game.getPacmanCurrentNodeIndex(),game.getGhostLastMoveMade(ghost), Constants.DM.PATH));
+				else
+				{
+						// attack Ms. Pacman with self written code.
+						ghostCurrentNodeIndex = game.getGhostCurrentNodeIndex(ghost);
+
+						MOVE move = getNextMoveAStar(game.getCurrentMaze().graph, pacManCurrentNodeIndex,
+							ghostCurrentNodeIndex);
+						myMoves.put(ghost, move);
+				}
+			}
+		}
 		
 		return myMoves;
+	}
+
+	public MOVE getNextMoveAStar(Node[] graph, int pacManCurrentNodeIndex, int
+		ghostCurrentNodeIndex) {
+
+
+
+		return MOVE.NEUTRAL;
+	}
+
+	//This helper function checks if Ms Pac-Man is close to an available power pill
+	private boolean closeToPower(Game game)
+	{
+		int[] powerPills=game.getPowerPillIndices();
+
+		for(int i=0;i<powerPills.length;i++)
+			if(game.isPowerPillStillAvailable(i) &&
+				 game.getShortestPathDistance(powerPills[i],game.getPacmanCurrentNodeIndex()) < PILL_PROXIMITY)
+				return true;
+
+		return false;
 	}
 }
